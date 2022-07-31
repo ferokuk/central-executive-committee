8d3a5522f83ea2617e417b00e12cad6a213a639f
ferokuk <gneushevyar@mail.ru>
2022-07-31 22:29:57 +0300

    version 0.1
    help

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '../views/HomePage.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomePage
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/HomePage.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="login">
+    <form @submit.prevent id = "loginForm">
+      Sign in to vote or propose votes
+      <br>
+      <input type="text" class="loginField" placeholder="Address">
+      <br>
+      <input type="password" class="loginField" placeholder="Password">
+      <br>
+      <button id="signInBtn">Sign in</button>
+    </form>
+    <p id = "signUpBtnTextAbove">First time here?</p>
+    <button id="signUpBtn">Sign up</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomePage',
+  components: {
+  }
+}
+</script>
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  font-family: poppins;
+}
+#loginForm{
+  font-size: 2rem;
+  color:black;
+}
+.loginField{
+  font-size: 1.5rem;
+  margin:1rem;
+  background-color: #CCCCFF;
+  width: 580px;
+  height: 50px;
+}
+input::placeholder{
+  color: gray;
+}
+#signInBtn{
+  font-size:2rem;
+  width:140px;
+  height:70px;
+  margin-top:1rem;
+  background-color: #CCCCFF;
+}
+#signUpBtn{
+  font-size:1.3rem;
+  margin-left:460px;
+  width:120px;
+  height:60px;
+  background-color: #CCCCFF;
+}
+#signUpBtnTextAbove{
+  font-size:1.3rem;
+  margin-left:470px;
+}
+#signUpBtn:hover{
+   background-color: #9999FF;
+   cursor: pointer;
+}
+#signInBtn:hover{
+   background-color: #9999FF;
+   cursor: pointer;
+}
+</style>
