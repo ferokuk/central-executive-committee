da20ba327492c27df74ecee8d879b8d4c925917b
Yaroslav <110298141+ferokuk@users.noreply.github.com>
2022-08-02 23:00:47 +0300

    version 1.0

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,25 +1,87 @@
 <template>
-  <div id="login">
+  <div id="login" v-if="isSignedIn===false">
     <form @submit.prevent id = "loginForm">
       Sign in to vote or propose votes
       <br>
-      <input type="text" class="loginField" placeholder="Address">
+      <input type="text" class="loginField" placeholder="Address" v-model="address" @input="addressChangeHandler">
       <br>
-      <input type="password" class="loginField" placeholder="Password">
+      <input type="password" class="loginField" placeholder="Password" v-model="password" @input="passwordChangeHandler" autocomplete="off">
       <br>
-      <button id="signInBtn">Sign in</button>
+      <button id="signInBtn" @click="signIn">Sign in</button>
     </form>
-    <p id = "signUpBtnTextAbove">First time here?</p>
-    <button id="signUpBtn">Sign up</button>
+   <RegistrationForm></RegistrationForm>
+  </div>
+  <div v-else>
+    <button @click="signOut" id = "signOutBtn">Sign out</button>
+    <ProfileView  :name="name" :address="address" :status="status"></ProfileView>
   </div>
 </template>
-
 <script>
+
+import ContractFunc from '@/contractWeb3'
+import w3 from '@/connectWeb3'
+import RegistrationForm from './RegistrationForm.vue'
+import ProfileView from './ProfileView.vue'
 
 export default {
   name: 'HomePage',
   components: {
-  }
+  },
+  data () {
+    return {
+      address: null,
+      password: null,
+      name: null,
+      status: null,
+      contract: null,
+      web3: null,
+      isSignedIn: false
+    }
+  },
+  async mounted () {
+    this.contract = await ContractFunc
+    this.web3 = w3()
+  },
+  methods: {
+    addressChangeHandler (event) {
+      this.address = event.target.value
+    },
+    passwordChangeHandler (event) {
+      this.password = event.target.value
+    },
+    async signIn () {
+      if(!this.web3.utils.isAddress(this.address)) {
+        alert("Please, check your address!")
+        return
+      }
+      await this.web3.eth.personal.unlockAccount(this.address,"").then(console.log("unlocked"))
+        await this.contract.methods
+        .login(this.address,this.password)
+        .send({from:this.address,gas:1000000})
+        .catch(error => alert(error))
+        .then(value => this.checkSuccess(value.events.LoginInformation.returnValues.information)) 
+      
+    },
+    async signOut(){
+        await this.contract.methods.logout().send({from:this.address}).then(value => this.checkSuccess(value.events.LoginInformation.returnValues.information))
+        //await this.web3.eth.personal.lockAccount(this.address).then(console.log('locked'))
+    },
+    async checkSuccess (_information) {
+      if(_information === 'Logged in successfully!'){
+        await this.contract.methods.Users(this.address).call().then(value => this.parseUserData (value.name,value.status))
+        this.isSignedIn = true
+      }
+      else if(_information === 'Logged out successfully!'){
+        await this.web3.eth.personal.lockAccount(this.address).then(console.log('locked'))
+        this.isSignedIn = false
+      }
+    },
+    parseUserData (_name,_status) {
+      this.name = _name
+      this.status = _status
+    }
+  },
+  components: { RegistrationForm, ProfileView}
 }
 </script>
 <style>
@@ -66,6 +128,16 @@ input::placeholder{
 }
 #signInBtn:hover{
    background-color: #9999FF;
+   cursor: pointer;
+}
+#signOutBtn{
+  font-size:1.3rem;
+  width:120px;
+  height:60px;
+  background-color: beige;
+}
+#signOutBtn:hover{
+   background-color:blanchedalmond;
    cursor: pointer;
 }
 </style>
---
+++ src/views/RegistrationForm.vue
@@ -0,0 +1,153 @@
+<template>
+     <button id="signUpBtn" @click="showRegistrationForm">Sign up</button>
+     <div v-if="openForm" class="form">
+        <div class="form-content">
+            <form @submit.prevent>
+              <button id="closeFormBtn" @click="showRegistrationForm">Close</button>
+              <br>
+              <input type="text" placeholder="Your name" required v-model="name">
+              <br>
+              <input type="text" placeholder="Your address" required v-model="address">
+              <br>
+              <input type="password" placeholder="Your password" required v-model="password" autocomplete="off">
+              <br>
+              <div class="status-picker">
+              <input type="radio" name="status" value="Elder" id="elder" required v-model="status">
+              <label for="elder">Elder</label>
+              <input type="radio" name="status" value="Member" id="member" required v-model="status">
+              <label for="member">Member</label>
+              <br>
+              </div>
+              <button id="createUserBtn" @click="signUp">Sign Up</button>
+          </form>
+        </div>
+     </div>
+</template>
+
+<script>
+import ContractFunc from '@/contractWeb3'
+import w3 from '@/connectWeb3'
+export default {
+  name: 'RegistrationForm',
+  data () {
+    return {
+      address: null,
+      password: null,
+      name: null,
+      status: null,
+      openForm: false,
+      web3: null,
+    }
+  },
+  async mounted () {
+    this.contract = await ContractFunc
+    this.web3 = w3()
+    
+  },
+  methods: {
+    showRegistrationForm () {
+      this.openForm = !this.openForm
+    },
+    addressChangeHandler (event) {
+      this.address = event.target.value
+    },
+    passwordChangeHandler (event) {
+      this.password = event.target.value
+    },
+    nameChangeHandler (event) {
+      this.name = event.target.value
+    },
+    statusChangeHandler (event) {
+      this.status = event.target.value
+    },
+   async signUp () {
+      if(!this.web3.utils.isAddress(this.address)){
+        alert("Please, check your address!")
+        return
+      }
+      try{
+      this.showRegistrationForm()
+      await this.web3.eth.personal
+      .unlockAccount(this.address,"")
+      .then(console.log("unlocked"))
+
+      await this.contract.methods
+      .createUser(this.name,this.password,this.address,this.status)
+      .send({from:this.address,gas:300000})
+      .then(console.log("created"))
+      this.address = this.password = this.name = this.status = null
+      }catch (e) {
+        alert(e)
+        await this.web3.eth.personal.lockAccount(this.address)
+        return
+      }
+      await this.web3.eth.personal.lockAccount(this.address)
+    }
+  }
+}
+</script>
+
+<style scoped>
+#createUserBtn{
+  font-size:1.3rem;
+  width:120px;
+  height:60px;
+  background-color: #CCCCFF;
+}
+#createUserBtn:hover{
+   background-color: #9999FF;
+   cursor: pointer;
+}
+#closeFormBtn{
+  font-size:1.3rem;
+  width:120px;
+  height:60px;
+  background-color: #CCCCFF;
+  top: 0;
+  left:0;
+  margin-top: 10px;
+  margin-bottom: 20px;
+  margin-right: 484px;
+}
+#closeFormBtn:hover{
+   background-color: #9999FF;
+   cursor: pointer;
+}
+.form{
+    top:0;
+    bottom:0;
+    right:0;
+    left:0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: rgba(0,0,0,0.5);
+    position: fixed;
+}
+.form-content{
+    margin: auto;
+    background: white;
+    border-radius: 10px;
+    min-height: 310px;
+    min-width: 600px;
+    padding: 25px;
+}
+input{
+  font-size: 1.8rem;
+  background-color: #CCCCFF;
+  width: 600px;
+  height: 50px;
+  margin: auto;
+  display: block;
+  font-style: poppins;
+}
+input::placeholder{
+  color: gray;
+}
+.status-picker{
+    justify-content: center;
+    align-items: center;
+    margin:0;
+    padding: 0;
+}
+</style>
